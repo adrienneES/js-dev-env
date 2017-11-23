89d7c6911cecc246fda511da69acf900875d3fd2
Adri Skrtic <adri.curtis@gmail.com>
2017-11-23 13:31:04 -0600

    added linting to framework

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
 import numeral from 'numeral';
-debugger;
+debugger; // eslint-disable-line no-debugger
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this class`);
+console.log(`I would pay ${courseValue} for this class`); // eslint-disable-line no-console
