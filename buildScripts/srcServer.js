17bea7ea8206d41f5e02ec98b15f8268114c12b8
Adri Skrtic <adri.curtis@gmail.com>
2017-11-26 12:09:07 -0600

    added testing to framework

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,9 +18,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+// adding /users route
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"callie","lastName":"skrtic","email":"c@c.com"},
+    {"id": 2,"firstName":"milo","lastName":"skrtic","email":"m@c.com"},
+    {"id": 3,"firstName":"nicholas","lastName":"skrtic","email":"n@c.com"},
+    {"id": 4,"firstName":"alex","lastName":"skrtic","email":"a@c.com"}
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
-    console.log(err);
+    console.log(err); // eslint-disable-line no-console
   }
   else {
     open('http://localhost:' + port);
